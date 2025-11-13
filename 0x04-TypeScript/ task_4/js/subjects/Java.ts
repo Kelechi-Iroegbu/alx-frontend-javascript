415422d46f4bf83667975e9cceaed1b8c466edf7
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2025-11-13 15:36:22 +0100

    Update Java.ts

--- 0x04-TypeScript/ task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/ task_4/js/subjects/Java.ts
@@ -1,1 +1,22 @@
+// Java.ts
+import { Subjects } from './Teacher';
+import './Subject';
 
+export namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
