b477e766f20307bb72714c78a621b7b6813952b2
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2025-11-13 15:52:53 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,12 +1,11 @@
-// Subject.ts
+// task_4/js/Subject.ts
 import { Subjects } from './Teacher';
 
 export namespace Subjects {
   export class Subject {
     teacher?: Teacher;
 
-    // 👇 Explicit method signature required by the task
-    setTeacher(teacher: Teacher) {
+    setTeacher(teacher:Teacher) {
       this.teacher = teacher;
     }
   }
