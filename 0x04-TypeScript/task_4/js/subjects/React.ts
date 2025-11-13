40fd1865d566a9461ab7369656387a65f19c6f63
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2025-11-13 15:33:23 +0100

    Update React.ts

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,1 +1,22 @@
+/ React.ts
+import { Subjects } from './Teacher';
+import './Subject';
 
+export namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
